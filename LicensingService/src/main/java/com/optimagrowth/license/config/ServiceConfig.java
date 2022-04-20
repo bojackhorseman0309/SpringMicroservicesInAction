875f1fb71d1c70b0caecb96dad9c1ced4b95f443
Bojack Horseman <aloaraya@outlook.com>
2022-04-19 21:05:59 -0600

    Chapter 5 ConfigServer

---
+++ LicensingService/src/main/java/com/optimagrowth/license/config/ServiceConfig.java
@@ -0,0 +1,15 @@
+package com.optimagrowth.license.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "example")
+public class ServiceConfig {
+
+    private String property;
+
+    public String getProperty() {
+        return property;
+    }
+
+
+}
